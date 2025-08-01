ccd44953a3f08950d81898682f67dbbaa03f6ddb
RajSinghania1 <rajblogger1990@gmail.com>
2025-08-01 14:34:58 +0530

    Fix Metro bundler configuration

--- metro.config.js
+++ metro.config.js
@@ -4,19 +4,8 @@ const config = getDefaultConfig(__dirname);
 
 // Ensure proper handling of web builds
 config.resolver.platforms = ['native', 'web', 'ios', 'android'];
-config.resolver.enableSymlinks = true;
 
 // Add support for additional file extensions
 config.resolver.sourceExts.push('cjs');
-
-// Optimize for web builds
-if (process.env.EXPO_PLATFORM === 'web') {
-  config.transformer.minifierConfig = {
-    keep_fnames: true,
-    mangle: {
-      keep_fnames: true,
-    },
-  };
-}
 
 module.exports = config;
